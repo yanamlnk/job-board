e314b7925645d27c72ae9a746fe948b05244c5c7
axelhuguet33 <axelh33@hotmail.fr>
2024-10-08 15:56:21 +0200

    creation of component article

--- frontend/src/components/button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles/Button.module.css";
-
-function Button () {
-  return <button className={styles.button}>Click Me</button>;
-};
-
-export default Button;
