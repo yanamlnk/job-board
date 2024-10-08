fa55f523d7614e926a322829e7a7ae5e08a0e259
axelhuguet33 <axelh33@hotmail.fr>
2024-10-08 09:43:50 +0200

    second test balise <p>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Bonjour bonjour bonjour</p>
+        <p>seconde balise p</p>
         <a
           className="App-link"
           href="https://reactjs.org"
