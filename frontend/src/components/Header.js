201f39252271f197c02c65667aa36417bb8426ea
YanaMlnk <yanamelnyk1998@gmail.com>
2024-10-08 16:38:09 +0200

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Logo from '../resources/Icon.png'
 import '../styles/Header_module.css'
-import Person from '../resources/personal_space.png'
-import Logout from '../resources/logout.png'
 
 const Header = () => {
   return (
     <header>
       <img src={Logo} alt='Logo'></img>
-      <button><img src = {Person} alt = "person icon"></img></button>
-      <button><img src = {Logout} alt = "logout icon"></img></button>
+      <div className="navigation-buttons">
+        <button><span className="material-symbols-outlined">person</span></button>
+        <button><span className="material-symbols-outlined">logout</span></button>
+      </div>
     </header>
   )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Article from "../components/Article";
 import "../styles/Home.css";
+import Header from "../components/Header";
+
 function Home() {
   return (
     <div className={"home"}>
       <div className={"container_articles"}>
+        <Header />
         <Article />
         <Article />
         <Article />
