61ea71573c0d19da379928f7c9bade59db3629a5
axelhuguet33 <axelh33@hotmail.fr>
2024-10-16 11:21:47 +0200

    redirecting path and design

---
+++ frontend/src/pages/Client.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ClientEdit from "../components/ClientEdit";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Client() {
+  return (
+    <div>
+      <Header />
+      <ClientEdit />
+      <Footer />
+    </div>
+  );
+}
+
+export default Client;
